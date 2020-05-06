0087ccf2c48cf2203f84c52ce47df4d5f853d2b0
Kartik Narang <bewithkartik@gmail.com>
2020-05-06 03:15:05 -0400

    Create process1.cpp

---
+++ process1.cpp
@@ -0,0 +1,31 @@
+#include<stdio.h>
+#include<netinet/in.h>
+#include<sys/types.h>
+#include<stdlib.h>
+#include<string.h>
+#include<iostream>
+#include<iterator>
+#include<arpa/inet.h>
+#include<unistd.h>
+#include "basics.cpp"
+using namespace std;
+char p_id[100]="process1";
+int main()
+{
+  message sendmsg,revmes;
+  client p;
+  p.create(1);
+  revmes.message_recv(p.s);
+  revmes.message_print();
+  sendmsg.message_read(1,2,101);
+  sendmsg.message_print();
+  sendmsg.message_send(p.s);
+  //do{fgets(sendmsg,20,stdin);
+  //int len=strlen(sendmsg);
+  //sendmsg[len-1]='\0';
+  //send(s,sendmsg,20,0);
+  //recv(s,revmes,100,0);
+  //revmes.message_print();
+  int cstatus=close(p.s);
+  return(0);
+}
